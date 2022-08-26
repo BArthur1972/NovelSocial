3d29a4bfa58bb25763cce60dd48d8c9283cc1eb9
Bethel Arthur <kbarthur14@gmail.com>
2022-08-26 18:11:23 -0500

    Add Log out button to ActionBar

--- app/src/main/java/com/example/novelsocial/MainActivity.java
+++ app/src/main/java/com/example/novelsocial/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.novelsocial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.mi_logout){
+            goToLoginActivity();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logOut() {
+        ParseUser.logOut();
+    }
+
+    private void goToLoginActivity() {
+        logOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
