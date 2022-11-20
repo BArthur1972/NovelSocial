fdcb97f31bafe50738e7d5e74ba6756980a4fad0
Bethel Arthur <kbarthur14@gmail.com>
2022-11-20 11:00:54 -0600

    Disable add to library button if the book has already been added

--- app/src/main/java/com/example/novelsocial/BookDetailsActivity.java
+++ app/src/main/java/com/example/novelsocial/BookDetailsActivity.java
@@ -18,21 +18,27 @@ import com.example.novelsocial.databinding.ActivityBookDetailsBinding;
 import com.example.novelsocial.models.Book;
 import com.example.novelsocial.models.LibraryItem;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
 public class BookDetailsActivity extends AppCompatActivity {
 
+    ActivityBookDetailsBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityBookDetailsBinding binding = ActivityBookDetailsBinding.inflate(getLayoutInflater());
+        binding = ActivityBookDetailsBinding.inflate(getLayoutInflater());
 
         // Root of the layout
         View view = binding.getRoot();
@@ -44,51 +50,56 @@ public class BookDetailsActivity extends AppCompatActivity {
         // Add the back button in the ActionBar to go back a previous page
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        // Add book to Library
-        Button addBookToLibrary = binding.btAddToLibrary;
-        addBookToLibrary.setOnClickListener(v -> {
-            ParseUser currentUser = ParseUser.getCurrentUser();
-            LibraryItem libraryItem = new LibraryItem(book);
+        // Add book to Library if it is has not already been added
+        // The button will only show if the book has not been added
+        if (book != null) {
+            displayFavouriteButton(book);
 
-            // Set item to the current user
-            libraryItem.setOwner(currentUser);
+            Button addBookToLibrary = binding.btAddToLibrary;
+            addBookToLibrary.setOnClickListener(v -> {
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                LibraryItem libraryItem = new LibraryItem(book);
 
-            libraryItem.saveInBackground(e -> {
-                if (e != null) {
-                    Toast.makeText(BookDetailsActivity.this, "Failed to add book to Library", Toast.LENGTH_SHORT).show();
-                    Log.e(getClass().getSimpleName(), "Failed to add!");
-                    return;
-                }
-                currentUser.add("LibraryItem", libraryItem);
-                currentUser.saveInBackground();
-                Toast.makeText(BookDetailsActivity.this, "The Book has been added to your Library", Toast.LENGTH_SHORT).show();
+                // Set item to the current user
+                libraryItem.setOwner(currentUser);
+
+                libraryItem.saveInBackground(e -> {
+                    if (e != null) {
+                        Toast.makeText(BookDetailsActivity.this, "Failed to add book to Library", Toast.LENGTH_SHORT).show();
+                        Log.e(getClass().getSimpleName(), "Failed to add!");
+                        return;
+                    }
+                    currentUser.add("LibraryItem", libraryItem);
+                    currentUser.saveInBackground();
+                    Toast.makeText(BookDetailsActivity.this, "The Book has been added to your Library", Toast.LENGTH_SHORT).show();
+                });
             });
-        });
 
-        // Navigate to WebView Activity when button is clicked
-        Button toWebViewActivity = findViewById(R.id.bt_web_view_activity);
+            // Find all components we want to populate
+            ImageView bookImageCover = binding.ivBookCoverView;
+            TextView bookTitle = binding.tvBookTitle;
+            TextView bookAuthor = binding.tvAuthorName;
+            TextView bookPublisher = binding.tvPublisherName;
+            TextView bookGenre = binding.tvGenre;
+            TextView bookPages = binding.tvNoOfPages;
 
-        toWebViewActivity.setOnClickListener(v -> goToWebViewActivity((book.getISBN()), book.getOpenLibraryId()));
+            // Set Book Cover in layout and remaining views with their respective values
+            Picasso.get()
+                    .load(book.getBookCoverUrl())
+                    .placeholder(R.drawable.ic_nocover)
+                    .into(bookImageCover);
 
-        // Find all components we want to populate
-        ImageView bookImageCover = binding.ivBookCoverView;
-        TextView bookTitle = binding.tvBookTitle;
-        TextView bookAuthor = binding.tvAuthorName;
-        TextView bookPublisher = binding.tvPublisherName;
-        TextView bookGenre = binding.tvGenre;
-        TextView bookPages = binding.tvNoOfPages;
+            bookTitle.setText(book.getTitle());
+            bookAuthor.setText(String.format("Author: %s", book.getAuthor()));
+            bookPublisher.setText(String.format("Publisher: %s", book.getPublisher()));
+            bookGenre.setText(String.format("Genre: %s", book.getGenre()));
+            bookPages.setText(String.format(Locale.ENGLISH, "No. Of Pages: %d", book.getPages()));
 
-        // Set Book Cover in layout and remaining views with their respective values
-        Picasso.get()
-                .load(book.getBookCoverUrl())
-                .placeholder(R.drawable.ic_nocover)
-                .into(bookImageCover);
+            // Navigate to WebView Activity when button is clicked
+            Button toWebViewActivity = binding.btWebViewActivity;
 
-        bookTitle.setText(book.getTitle());
-        bookAuthor.setText(String.format("Author: %s", book.getAuthor()));
-        bookPublisher.setText(String.format("Publisher: %s", book.getPublisher()));
-        bookGenre.setText(String.format("Genre: %s", book.getGenre()));
-        bookPages.setText(String.format(Locale.ENGLISH, "No. Of Pages: %d", book.getPages()));
+            toWebViewActivity.setOnClickListener(v -> goToWebViewActivity((book.getISBN()), book.getOpenLibraryId()));
+        }
     }
 
     @Override
@@ -110,6 +121,21 @@ public class BookDetailsActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void displayFavouriteButton(Book book) {
+        ParseQuery<LibraryItem> query = new ParseQuery<>(LibraryItem.class);
+        query.whereEqualTo("bookId", String.valueOf(book.getOpenLibraryId()));
+        query.findInBackground(new FindCallback<LibraryItem>() {
+            @Override
+            public void done(List<LibraryItem> objects, ParseException e) {
+                if (e == null) {
+                    if (objects.size() == 0) {
+                        binding.btAddToLibrary.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
     }
 
     public void goToWebViewActivity(String isbn, String openLibraryId) {
